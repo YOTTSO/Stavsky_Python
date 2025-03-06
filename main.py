c94be941ac4d5d597efeefc659e743b73a2a035c
yottso <illya.stavsky.2004@gmail.com>
2025-03-06 12:13:22 +0300

    updated project with interactive input

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
+import os
 import time
 
-user_input = input('Enter 7, John or numeric array')
-
-try:
-    if int(user_input) > 7:
-        print('Hello')
-except ValueError:
+while True:
+    print('Type \'exit\' to quit')
+    user_input = input('Enter 7, John or numeric array')
+    if user_input == 'exit':
+        exit()
     try:
-        if user_input == 'John':
-            print('Hello, John')
+        if int(user_input) > 7:
+            print('Hello')
     except ValueError:
-        print('There is no such name')
-    try:
-        numbers = list(map(int, user_input.split(",")))
-        specific_list = [x for x in numbers if x % 3 == 0]
-        print(f'{specific_list}')
-    except ValueError:
-        pass
-time.sleep(10)
+        try:
+            if user_input == 'John':
+                print('Hello, John')
+        except ValueError:
+            print('There is no such name')
+        try:
+            numbers = list(map(int, user_input.split(",")))
+            specific_list = [x for x in numbers if x % 3 == 0]
+            print(f'{specific_list}')
+        except ValueError:
+            pass
+    time.sleep(1)
+    os.system('cls' if os.name == 'nt' else 'clear')
